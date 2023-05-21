6c9de68735e93e3eaf65e9de3aa76ec2f4accee6
Nicole Lam <Nicolelam8891@gmail.com>
2023-05-21 12:18:45 -0600

    Filter for numbers below 60

--- friends.js
+++ friends.js
@@ -1,6 +1,12 @@
 var numbers = [8, 72, 36, 14, 22, 35, 82, 55];
+// for (var i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 30) {
+//         console.log(numbers[i]);
+//     }
+// }
+
 for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] > 30) {
+    if (numbers[i] < 60) {
         console.log(numbers[i]);
     }
 }
